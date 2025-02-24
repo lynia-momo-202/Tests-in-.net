6784489a59b7c574484ddb6d714fda5a5cb98ccc
lynia-momo-202 <lyniamomo@gmail.com>
2025-02-24 10:38:01 +0100

    chore(tests):rename test method

---
+++ Julio Casal - C# Unit Testing Essentials/GameLibrary/GameLibrary.UnitTests/TreasureChestTests.cs
@@ -0,0 +1,18 @@
+﻿namespace GameLibrary.UnitTests
+{
+    public class TreasureChestTests
+    {
+        //MethodName_StateUnderTest_ExpectedBehavior
+
+        [Fact]
+        public void CanOpen_ChestIsLockedAndHasKey_ReturnsTrue()
+        {
+            
+            var chest = new TreasureChest(true);
+
+            var result = chest.CanOpen(true);
+
+            Assert.True(result);
+        }
+    }
+}
