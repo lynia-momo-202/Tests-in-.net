077a73638ec2ca1542d4ae2d36f7c2c1b3178163
lynia-momo-202 <lyniamomo@gmail.com>
2025-03-06 16:24:28 +0100

    chore( test): ignoring test

--- Julio Casal - C# Unit Testing Essentials/GameLibrary/GameLibrary.UnitTests/TreasureChestTests.cs
+++ Julio Casal - C# Unit Testing Essentials/GameLibrary/GameLibrary.UnitTests/TreasureChestTests.cs
@@ -39,69 +39,69 @@ namespace GameLibrary.UnitTests
             Assert.Single(chests);
         }
 
-        //[Fact]
-        //public void CanOpen_ChestIsLockedAndHasKey_ReturnsTrue()
-        //{
-        //    // Arange
-        //    var sut = new TreasureChest(true);
-        //    chests.Push(sut);//ajouter le system test dans la pile
-        //    output.WriteLine($"Chest count: {chests.Count}");
+        [Fact (Skip = "This test is not ready yet")]
+        public void CanOpen_ChestIsLockedAndHasKey_ReturnsTrue()
+        {
+            // Arange
+            var sut = new TreasureChest(true);
+            chests.Push(sut);//ajouter le system test dans la pile
+            output.WriteLine($"Chest count: {chests.Count}");
 
-        //    // Act
-        //    var result = sut.CanOpen(true);
+            // Act
+            var result = sut.CanOpen(true);
 
-        //    // Assert
-        //    Assert.True(result);
-        //    Assert.Single(chests);//verifier qu'il y a qu'un seul elmt dans la pile
-        //}
+            // Assert
+            Assert.True(result);
+            Assert.Single(chests);//verifier qu'il y a qu'un seul elmt dans la pile
+        }
 
-        //[Fact]
-        //public void ConOpen_ChestIsLockedAndHasNoKey_ReturnsFalse()
-        //{
-        //    // Arrange
-        //    var sut = new TreasureChest(true);
-        //    chests.Push(sut);
-        //    output.WriteLine($"Chest count: {chests.Count}");
+        [Fact(Skip = "This test is not ready yet")]
+        public void ConOpen_ChestIsLockedAndHasNoKey_ReturnsFalse()
+        {
+            // Arrange
+            var sut = new TreasureChest(true);
+            chests.Push(sut);
+            output.WriteLine($"Chest count: {chests.Count}");
 
-        //    // Act
-        //    var result = sut.CanOpen(false);
+            // Act
+            var result = sut.CanOpen(false);
 
-        //    // assert
-        //    Assert.False(result);
-        //    Assert.Single(chests);
-        //}
+            // assert
+            Assert.False(result);
+            Assert.Single(chests);
+        }
 
-        //[Fact]
-        //public void CanOpen_ChestInUnLockedAndHasKey_ReturnsTrue()
-        //{
-        //    //Arrange
-        //    var sut = new TreasureChest(false);
-        //    chests.Push(sut);
-        //    output.WriteLine($"Chest count: {chests.Count}");
+        [Fact(Skip = "This test is not ready yet")]
+        public void CanOpen_ChestInUnLockedAndHasKey_ReturnsTrue()
+        {
+            //Arrange
+            var sut = new TreasureChest(false);
+            chests.Push(sut);
+            output.WriteLine($"Chest count: {chests.Count}");
 
-        //    //Act
-        //    var result = sut.CanOpen(true);
+            //Act
+            var result = sut.CanOpen(true);
 
-        //    //assert
-        //    Assert.True(result);
-        //    Assert.Single(chests);
-        //}
+            //assert
+            Assert.True(result);
+            Assert.Single(chests);
+        }
 
-        //[Fact]
-        //public void CanOpen_ChestInUnLockedAndHasNoKey_ReturnsTrue()
-        //{
-        //    //Arrange
-        //    var sut = new TreasureChest(false);
-        //    chests.Push(sut);
-        //    output.WriteLine($"Chest count: {chests.Count}");
+        [Fact(Skip = "This test is not ready yet")]
+        public void CanOpen_ChestInUnLockedAndHasNoKey_ReturnsTrue()
+        {
+            //Arrange
+            var sut = new TreasureChest(false);
+            chests.Push(sut);
+            output.WriteLine($"Chest count: {chests.Count}");
 
-        //    //Act
-        //    var result = sut.CanOpen(false);
+            //Act
+            var result = sut.CanOpen(false);
 
-        //    //assert
-        //    Assert.True(result);
-        //    Assert.Single(chests);
-        //}
+            //assert
+            Assert.True(result);
+            Assert.Single(chests);
+        }
 
         public void Dispose()
         {
